33552849d79fdfc7084037cde8f46f1bc11ef68c
Raphealkxy <1277609958@qq.com>
2017-08-21 18:27:57 +0800

    add project

---
+++ src/com/timmy/structs/Action.java
@@ -0,0 +1,23 @@
+package com.timmy.structs;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Action extends ActionSupport{
+
+	@Override
+	public String execute() throws Exception {
+		return "ok";
+	}
+	public String add()
+	{
+		return "ok";
+	}
+	public String delete()
+	{
+		return "ok";
+	}
+
+
+	
+	
+}
